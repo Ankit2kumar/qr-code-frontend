232de209e52c4776f5c5973942f61ede63e22d54
Ankit Kumar <ankitlucky.kumar99@gmail.com>
2023-12-19 10:48:42 +0100

    Initial commits

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+/** @format */
+
+import './App.css';
+
+function App() {
+	return (
+		<>
+			<div className="container bg-black-800">Hello</div>
+			<div className="grid grid-cols-3 gap-4">
+				<div className="bg-blue-500 p-4">Item 1</div>
+				<div className="bg-green-500 p-4">Item 2</div>
+				<div className="bg-red-500 p-4">Item 3</div>
+				<div className="bg-yellow-500 p-4">Item 4</div>
+				<div className="bg-purple-500 p-4">Item 5</div>
+				<div className="bg-orange-500 p-4">Item 6</div>
+			</div>
+		</>
+	);
+}
+
+export default App;
