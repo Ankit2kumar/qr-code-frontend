52907cafdea5df7a670372fedb41e6e26fa1baa7
Ankit Kumar <ankitlucky.kumar99@gmail.com>
2023-12-20 17:00:28 +0100

    Adds SignIn page

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -3,7 +3,40 @@
 import React from 'react';
 
 const SignIn: React.FC = () => {
-	return <>hello</>;
+	return (
+		<>
+			<div className="flex flex-row min-h-[80vh]  justify-center items-center">
+				<div className="w-[460px]  bg-slate-300 rounded-xl">
+					<form className="mx-auto py-8 flex flex-col gap-5 w-[330px]">
+						<h3 className="text-center font-bold text-[#1E0E62] text-4xl mb-6">
+							Sign In
+						</h3>
+						<input
+							type="email"
+							name="email"
+							id="email"
+							placeholder="Email"
+							required
+							className="pl-2 py-2 rounded-lg "
+						/>
+						<input
+							type="password"
+							name="password"
+							id="password"
+							placeholder="password"
+							required
+							className="pl-2 py-2 rounded-lg"
+						/>
+
+						<button className="bg-indigo-600 font-medium text-white rounded-xl p-3 text-center justify-center items-center">
+							{' '}
+							Sign In
+						</button>
+					</form>
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default SignIn;
